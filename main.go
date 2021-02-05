bbdac039ce34d28fb60100ec2229c15d025973b2
YohoYang <yohoyang@outlook.com>
2021-02-05 15:05:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -104,10 +104,10 @@ func index(c *gin.Context) {
 		if referer.Host == host { //站内referer自动通过免白名单
 			isForbidden = false;
 		}
-		if referer.Host == "www.xxxx.com" { //白名单，如果支持多个可能要改成循环
+		if referer.Host == "www.sbsub.com" { //白名单，如果支持多个可能要改成循环
 			isForbidden = false;
 		} 
-	} else if reqFullUrl == "www.xxxx.com" { //允许直接访问首页，但不允许直接引用文件，首页地址，不太熟悉go不知道怎么获取带https://的完整url，应该可以用config.GloablConfig.HerokuAppUrl
+	} else if reqFullUrl == "yoho-s1.herokuapp.com" { //允许直接访问首页，但不允许直接引用文件，首页地址，不太熟悉go不知道怎么获取带https://的完整url，应该可以用config.GloablConfig.HerokuAppUrl
 		isForbidden = false;
 	} else {
 		//空referer
