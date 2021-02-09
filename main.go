192575f6dcf3d9a3c1cb3dbfdee36f80f9defc17
YohoYang <yohoyang@outlook.com>
2021-02-09 11:56:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -121,6 +121,11 @@ func index(c *gin.Context) {
 		return
 	}
 	
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "GET")
+	c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+	c.Header("Access-Control-Allow-Credentials", "true")
 	tmpFile := strings.Join([]string{"189/", "/index.html"}, config.GloablConfig.Theme)
 	pwd := ""
 	pwdCookie, err := c.Request.Cookie("dir_pwd")
